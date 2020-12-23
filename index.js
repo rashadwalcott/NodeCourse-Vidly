237916b16d785f03d31f0d1eb6bbb15200dabcad
Rashad <rashad.walcott15@gmail.com>
2020-12-23 18:29:40 -0500

    Post and Put request

--- index.js
+++ index.js
@@ -5,25 +5,57 @@ const app = express();
 app.use(express.json());
 
 const genres = [
-  {
-    id: 1,
-    genre: "Horror",
-    id: 2,
-    genre: "Action",
-    id: 3,
-    genre: "Drama",
-  },
+  { id: 1, genre: "Horror" },
+  { id: 2, genre: "Action" },
+  { id: 3, genre: "Drama" },
 ];
 
 //GET All Genres
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
 
 //GET Genre with ID
-
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given id was not found");
+  res.send(genre);
+});
 //POST genre to list
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    genre: req.body.genre,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
 
 //UPDATE(PUT) Genre
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given id was not found");
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  genre.genre = req.body.genre;
+  res.send(genre);
+});
 
 //DELETE Genre with ID
+
+//Validate Genre
+function validateGenre(genre) {
+  const schema = Joi.object({ genre: Joi.string().min(3).required() });
+
+  return schema.validate(genre);
+}
 
 //PORT
 const port = process.env.PORT || 3000;
