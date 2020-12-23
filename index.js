075dd2022ba3999a75418ba308a7943d006d3c2b
Rashad <rashad.walcott15@gmail.com>
2020-12-23 17:55:50 -0500

    Added port

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ const genres = [
     genre: "Drama",
   },
 ];
+
+//GET All Genres
+
+//GET Genre with ID
+
+//POST genre to list
+
+//UPDATE(PUT) Genre
+
+//DELETE Genre with ID
+
+//PORT
+const port = process.env.PORT || 3000
+app.listen(port () => {
+    console.log(`Listening on port ${port}`);
+})
