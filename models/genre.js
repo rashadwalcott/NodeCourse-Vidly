63dd37965f22b5ddf674dfc55331014a18f9cf7f
Rashad <rashad.walcott15@gmail.com>
2020-12-29 19:08:31 -0500

    Added movie models and routes

--- models/genre.js
+++ models/genre.js
@@ -1,17 +1,16 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const Genre = mongoose.model(
-  'Genre',
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxLength: 50,
-    },
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxLength: 50,
+  },
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 //Validate Genre
 function validateGenre(genre) {
@@ -20,5 +19,6 @@ function validateGenre(genre) {
   return schema.validate(genre);
 }
 
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validate = validateGenre;
