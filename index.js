473ed34ed530321c64a66c840be34b54023ec93c
Rashad <rashad.walcott15@gmail.com>
2021-01-04 09:31:42 -0500

    Extracting the Db logic

--- index.js
+++ index.js
@@ -2,23 +2,16 @@ require('express-async-errors');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
 require('./startup/routes')(app);
+require('./startup/db')();
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
 }
-mongoose
-  .connect('mongodb://localhost:27017/vidly', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Could not connect to MongoDB...'));
 
 //PORT
 const port = process.env.PORT || 3000;
--- startup/routes.js
+++ startup/routes.js
@@ -7,7 +7,7 @@ const rentals = require('../routes/rentals');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
 
-model.exports = function (app) {
+module.exports = function (app) {
   app.use(express.json());
   app.use('/api/genres', genres);
   app.use('/api/customers', customers);
