23b16272deb0cddca4eccdba1d50d3987f2f6bde
Rashad <rashad.walcott15@gmail.com>
2021-01-04 09:21:39 -0500

    Extracting routes

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 require('express-async-errors');
-const winston = require('winston');
-const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const express = require('express');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const { log } = require('winston');
 const app = express();
 
-throw new Error('Something failed during startup.');
+require('./startup/routes')(app);
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
@@ -28,15 +19,6 @@ mongoose
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
 
 //PORT
 const port = process.env.PORT || 3000;
