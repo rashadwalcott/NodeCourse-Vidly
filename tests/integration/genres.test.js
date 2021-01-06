d3b2432728c9395e78c42f850be4770c751f522b
Rashad <rashad.walcott15@gmail.com>
2021-01-06 13:54:10 -0500

    Testing Routes with Parameters

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -27,6 +27,14 @@ describe('/api/genres', () => {
     });
   });
   describe('GET /:id', () => {
-    it('should return the genre that matches the id', () => {});
+    it('should return a genre if valid id is passed', async () => {
+      const genre = new Genre({ name: 'genre1' });
+      await genre.save();
+
+      const res = await request(server).get('/api/genres/' + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('name', genre.name);
+    });
   });
 });
