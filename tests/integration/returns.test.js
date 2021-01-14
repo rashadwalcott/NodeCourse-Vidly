167d3dd9de9d8a891ea0765b44e5d2d11b90437b
Rashad <rashad.walcott15@gmail.com>
2021-01-14 16:00:16 -0500

    Testing the Input

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const { Rental } = require('../../models/rentals');
+const { User } = require('../../models/user');
 const mongoose = require('mongoose');
 
 describe('/api/returns', () => {
@@ -40,5 +41,27 @@ describe('/api/returns', () => {
       .send({ customerId, movieId });
 
     expect(res.status).toBe(401);
+  });
+
+  it('should return 400 if customerId is not provided', async () => {
+    const token = new User().generateAuthToken();
+
+    const res = await request(server)
+      .post('/api/returns')
+      .set('x-auth-token', token)
+      .send({ movieId });
+
+    expect(res.status).toBe(400);
+  });
+
+  it('should return 400 if movieId is not provided', async () => {
+    const token = new User().generateAuthToken();
+
+    const res = await request(server)
+      .post('/api/returns')
+      .set('x-auth-token', token)
+      .send({ customerId });
+
+    expect(res.status).toBe(400);
   });
 });
