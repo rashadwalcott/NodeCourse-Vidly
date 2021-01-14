ead2707992864b98dee6edac43d6bfdf26aec6b9
Rashad <rashad.walcott15@gmail.com>
2021-01-14 16:35:53 -0500

    Lookung up an object

--- routes/returns.js
+++ routes/returns.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { Rental } = require('../models/rental');
 
 //GET All Genres
 router.post('/', async (req, res) => {
   if (!req.body.customerId)
     return res.status(400).send('customerId not provided');
+
   if (!req.body.movieId) return res.status(400).send('movieId not provided');
   res.status(401).send('Unauthorized');
+
+  const rental = await Rental.findOne({
+    'customer._id': req.body.customerId,
+    'movie._id': req.body.movieId,
+  });
+  if (!rental) return res.status(404).send('Rental not found.');
 });
 
 module.exports = router;
